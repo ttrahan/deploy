6e29929606a5effd364c6e5d90fd94ef17959a83
ragsden <raghu@shippable.com>
2015-10-29 04:50:23 -0700

    Update security-groups.tf

--- aye-aye/security-groups.tf
+++ aye-aye/security-groups.tf
@@ -41,9 +41,9 @@ resource "aws_security_group" "shippable_aye_aye_web_sg" {
   vpc_id = "${aws_vpc.default.id}"
   
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
