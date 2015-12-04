87762464c7a9afcf57bfe5e35879146bbdba4a67
Avi Cavale <avi@shippable.com>
2015-12-04 09:32:28 -0800

    commenting LBs

--- default/public0-0.tf
+++ default/public0-0.tf
@@ -132,69 +132,69 @@ resource "aws_security_group" "demoWebSG" {
 }
 
 # WWW Load balancer
-resource "aws_elb" "demoWWWLb" {
-
-  name = "demoWWWLb"
-  subnets = [
-    "${aws_subnet.demoPubSN0-0.id}"]
-  security_groups = [
-    "${aws_security_group.demoWebSG.id}"]
-
-  #run time a lot of things change here. So ignore
-  lifecycle {
-    ignore_changes = [
-      "listener",
-      "instances",
-      "health_check"
-    ]
-  }
-
-  listener {
-    instance_port = 80
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
-  }
-
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    target = "HTTP:80/"
-    interval = 5
-  }
-}
-
-# API Load balancer
-resource "aws_elb" "demoAAPILb" {
-
-  name = "demoAAPILb"
-  subnets = [
-    "${aws_subnet.demoPubSN0-0.id}"]
-  security_groups = [
-    "${aws_security_group.demoWebSG.id}"]
-
-  #run time a lot of things change here. So ignore
-  lifecycle {
-    ignore_changes = [
-      "listener",
-      "instances",
-      "health_check"
-    ]
-  }
-
-  listener {
-    instance_port = 80
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
-  }
-
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    target = "HTTP:80/"
-    interval = 5
-  }
-}
+//resource "aws_elb" "demoWWWLb" {
+//
+//  name = "demoWWWLb"
+//  subnets = [
+//    "${aws_subnet.demoPubSN0-0.id}"]
+//  security_groups = [
+//    "${aws_security_group.demoWebSG.id}"]
+//
+//  #run time a lot of things change here. So ignore
+////  lifecycle {
+////    ignore_changes = [
+////      "listener",
+////      "instances",
+////      "health_check"
+////    ]
+////  }
+//
+//  listener {
+//    instance_port = 50000
+//    instance_protocol = "http"
+//    lb_port = 50000
+//    lb_protocol = "http"
+//  }
+//
+//  health_check {
+//    healthy_threshold = 2
+//    unhealthy_threshold = 2
+//    timeout = 3
+//    target = "HTTP:80/"
+//    interval = 5
+//  }
+//}
+//
+//# API Load balancer
+//resource "aws_elb" "demoAAPILb" {
+//
+//  name = "demoAAPILb"
+//  subnets = [
+//    "${aws_subnet.demoPubSN0-0.id}"]
+//  security_groups = [
+//    "${aws_security_group.demoWebSG.id}"]
+//
+//  #run time a lot of things change here. So ignore
+////  lifecycle {
+////    ignore_changes = [
+////      "listener",
+////      "instances",
+////      "health_check"
+////    ]
+////  }
+//
+//  listener {
+//    instance_port = 50000
+//    instance_protocol = "http"
+//    lb_port = 50000
+//    lb_protocol = "http"
+//  }
+//
+//  health_check {
+//    healthy_threshold = 2
+//    unhealthy_threshold = 2
+//    timeout = 3
+//    target = "HTTP:80/"
+//    interval = 5
+//  }
+//}
