a70323358b1b9bc587cbeee308ec9dc4e1c89acf
ttrahan <tom@shippable.com>
2015-12-08 17:30:18 -0800

    update default availability zone to us-east-1a
    us-east-1d was invalid for creating a subnet.

--- default/variables.tf
+++ default/variables.tf
@@ -14,7 +14,7 @@ variable "region" {
 
 variable "availability_zone" {
   description = "availability zone used for the demo"
-  default = "us-east-1d"
+  default = "us-east-1a"
 }
 
 # Special AMI for ECS container Service
