9ba0018a033fc2264d7b3377ebfa350369ef505a
Tom Trahan <tom@shippable.com>
2016-02-04 16:52:23 -0800

    update description

--- default/variables.tf
+++ default/variables.tf
@@ -13,7 +13,7 @@ variable "region" {
 }
 
 variable "availability_zone" {
-  description = "availability zone used for the demo"
+  description = "availability zone used for the demo, based on region"
   default = {
     us-east-1 = "us-east-1a"
     us-west-1 = "us-west-1a"
